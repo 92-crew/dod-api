fa4c83cc6bc24bf686a14638b288171166bbe819
hunao3o <siamm0528@gmail.com>
2021-04-11 22:54:45 +0900

    #1 [api] project init - Set member-meta - Set Hello components (HelloController, HelloProvider, ..)

--- dod-member/member-api/src/main/java/com/crew92/doordie/member/api/controller/HelloController.java
+++ dod-member/member-api/src/main/java/com/crew92/doordie/member/api/controller/HelloController.java
@@ -1,14 +1,24 @@
 package com.crew92.doordie.member.api.controller;
 
+import com.crew92.doordie.member.domain.provider.HelloProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final HelloProvider helloProvider;
 
     @GetMapping("/hello")
     public String hello() {
         return "hello. member-api";
+    }
+
+    @GetMapping("/helloMeta")
+    public String helloMeta() {
+        return helloProvider.getHello().toString();
     }
 
 }
