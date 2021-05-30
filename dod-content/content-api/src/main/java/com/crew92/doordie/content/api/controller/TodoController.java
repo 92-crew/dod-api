3f13c71a8522cba28f336ea7545817d36d12cf13
김현아 <hyeona@wemakeprice.com>
2021-05-30 23:08:43 +0900

    #39 content-api deploy - security 설정

--- dod-content/content-api/src/main/java/com/crew92/doordie/content/api/controller/TodoController.java
+++ dod-content/content-api/src/main/java/com/crew92/doordie/content/api/controller/TodoController.java
@@ -4,12 +4,22 @@ import com.crew92.doordie.content.api.dto.todo.TodoDto;
 import com.crew92.doordie.content.api.dto.todo.TodosDto;
 import com.crew92.doordie.content.api.exception.InvalidMemberIdException;
 import com.crew92.doordie.content.api.service.TodoService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
@@ -42,13 +52,13 @@ public class TodoController {
     }
 
     private static class HeaderNames {
-
         public static final String MEMBER_ID = "x-dod-mid";
     }
 
     @ExceptionHandler(InvalidMemberIdException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public String invalidMemberIdException(InvalidMemberIdException e) {
+        log.error("InvalidMemberIdException caused!! message: {}", e.getMessage());
         throw e;
     }
 
