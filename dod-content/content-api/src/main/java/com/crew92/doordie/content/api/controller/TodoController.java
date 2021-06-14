5f626bc305e9c1e89d46c1477be0bf34ded615df
김현아 <hyeona@wemakeprice.com>
2021-06-14 22:22:04 +0900

    #43 [content-api] URI version path 위치 변경

--- dod-content/content-api/src/main/java/com/crew92/doordie/content/api/controller/TodoController.java
+++ dod-content/content-api/src/main/java/com/crew92/doordie/content/api/controller/TodoController.java
@@ -2,21 +2,17 @@ package com.crew92.doordie.content.api.controller;
 
 import com.crew92.doordie.content.api.dto.todo.TodoDto;
 import com.crew92.doordie.content.api.dto.todo.TodosDto;
-import com.crew92.doordie.content.api.exception.InvalidMemberIdException;
 import com.crew92.doordie.content.api.service.TodoService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -26,40 +22,33 @@ public class TodoController {
 
     private final TodoService todoService;
 
-    @GetMapping("/api/content/v1/todo/{id}")
+    @GetMapping("/api/v1/content/todo/{id}")
     public TodoDto getById(@PathVariable Long id) {
         return todoService.getById(id);
     }
 
-    @GetMapping("/api/content/v1/todos")
+    @GetMapping("/api/v1/content/todos")
     public TodosDto getByMemberId(@RequestHeader(HeaderNames.MEMBER_ID) Long memberId) {
         return todoService.getByMemberId(memberId);
     }
 
-    @PostMapping("/api/content/v1/todo")
+    @PostMapping("/api/v1/content/todo")
     public TodoDto create(@RequestHeader(HeaderNames.MEMBER_ID) Long memberId, @RequestBody @Valid TodoCreateCondition condition) {
         return todoService.create(memberId, condition);
     }
 
-    @PutMapping("/api/content/v1/todo")
+    @PutMapping("/api/v1/content/todo")
     public TodoDto update(@RequestHeader(HeaderNames.MEMBER_ID) Long memberId, @RequestBody @Valid TodoUpdateCondition condition) {
         return todoService.update(memberId, condition);
     }
 
-    @DeleteMapping("/api/content/v1/todo/{id}")
+    @DeleteMapping("/api/v1/content/todo/{id}")
     public void delete(@PathVariable Long id) {
         todoService.removeById(id);
     }
 
     private static class HeaderNames {
         public static final String MEMBER_ID = "x-dod-mid";
-    }
-
-    @ExceptionHandler(InvalidMemberIdException.class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public String invalidMemberIdException(InvalidMemberIdException e) {
-        log.error("InvalidMemberIdException caused!! message: {}", e.getMessage());
-        throw e;
     }
 
 }
