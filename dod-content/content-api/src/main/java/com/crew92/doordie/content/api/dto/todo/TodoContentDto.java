ed6a055440c1c405b889f6b6d781f76b51ae4dec
김현아 <hyeona@wemakeprice.com>
2021-06-21 22:20:53 +0900

    #52 [content-api] DateTime -> Korea 로 변경

--- dod-content/content-api/src/main/java/com/crew92/doordie/content/api/dto/todo/TodoContentDto.java
+++ dod-content/content-api/src/main/java/com/crew92/doordie/content/api/dto/todo/TodoContentDto.java
@@ -1,19 +1,12 @@
 package com.crew92.doordie.content.api.dto.todo;
 
+import static java.util.stream.Collectors.toList;
+
 import com.crew92.doordie.content.api.dto.ContentDto;
 import com.crew92.doordie.content.domain.repository.entity.TodoEntity;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.util.ObjectUtils;
-
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toList;
-import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Getter
 @Setter
