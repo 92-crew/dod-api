e40b2fb528c0e6d9de318431d24042cbb85951c9
김현아 <hyeona@wemakeprice.com>
2021-06-29 22:22:59 +0900

    #56 [content-api] todolist dueDate DESC 순으로 변경

--- dod-content/content-api/src/main/java/com/crew92/doordie/content/api/dto/todo/TodoContentDto.java
+++ dod-content/content-api/src/main/java/com/crew92/doordie/content/api/dto/todo/TodoContentDto.java
@@ -3,7 +3,9 @@ package com.crew92.doordie.content.api.dto.todo;
 import static java.util.stream.Collectors.toList;
 
 import com.crew92.doordie.content.api.dto.ContentDto;
+import com.crew92.doordie.content.api.utils.DateTimeUtils;
 import com.crew92.doordie.content.domain.repository.entity.TodoEntity;
+import java.util.Date;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,14 +14,16 @@ import lombok.Setter;
 @Setter
 public class TodoContentDto implements ContentDto {
 
+    private Date dueDate;
     private String dueDateString;
     private List<TodoDto> todos;
 
-    public static TodoContentDto of(String dueDateString, List<TodoEntity> entities) {
+    public static TodoContentDto of(Date dueDate, List<TodoEntity> entities) {
         TodoContentDto dto = new TodoContentDto();
         List<TodoDto> todos = entities.stream().map(TodoDto::transfer).collect(toList());
         dto.setTodos(todos);
-        dto.setDueDateString(dueDateString);
+        dto.setDueDate(dueDate);
+        dto.setDueDateString(DateTimeUtils.toString(dueDate));
 
         return dto;
     }
